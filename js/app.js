1632effbbf8c97887cb3b156799c51e4e3c6194b
ak1000 <alexanderhlebus@gmail.com>
2014-12-01 17:56:39 +0300

    Initial commit. Just Html, Css and a little bit AngularJs

---
+++ js/app.js
@@ -0,0 +1,67 @@
+﻿var ak = angular.module('ak', ['ak.managers', 'firebase', 'ui.bootstrap', 'ngRoute']);
+
+ak.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	$routeProvider.
+		when('/', {
+			templateUrl: 'modules/profile/profile.html',
+			controller: 'akController'
+		}).
+		when('/bjj', {
+			templateUrl: 'modules/bjj/bjj.html',
+			controller: 'akController'
+		}).
+		when('/profile', {
+			templateUrl: 'modules/profile/profile.html',
+			controller: 'akController'
+		}).
+		when('/food', {
+			templateUrl: 'modules/food/food.html',
+			controller: 'akController'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	$locationProvider.html5Mode(true);
+}]);
+
+ak.controller('akController', ['$scope', '$firebase', '$location', 'drillManager',
+	function ($scope, $firebase, $location, drillManager) {
+
+		$scope.resources = {
+
+		};
+
+		$scope.profile = {
+			name: 'Александр Хлебус'
+		};
+
+		$scope.bjj = {
+			name: 'Бразильское Джиу-Джитсу',
+			belt: 'Blue Belt',
+			drills: {
+				belts: {
+					blue: [
+						{
+							id: 0,
+							name: 'Trap and Roll',
+							count: 0,
+							range: { min: 5, max: 15 }
+						},
+						{
+							id: 1,
+							name: 'Armlock',
+							count: 0,
+							range: { min: 2, max: 7 }
+						}
+					]
+				}
+			}
+		};
+
+		$scope.handlers = {
+			drill: drillManager,
+			isActivePage: function (viewLocation) {
+				return viewLocation === $location.path();
+			}
+		};
+	}]);
---
+++ js/managers.js
@@ -0,0 +1,19 @@
+﻿var akManagers = angular.module('ak.managers', []);
+
+akManagers.factory('drillManager', function () {
+	
+	var drillManager = {
+		increment: function (drill) {
+			++drill.count;
+		},
+		decrement: function (drill) {
+			if (drill.count > 0) {
+				--drill.count;
+			} else {
+				drill.count = 0;
+			}
+		}
+	};
+
+	return drillManager;
+});
